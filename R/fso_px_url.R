7b64a3f39f8da9d3c8489f0d90feaa29dd1dcbbf
Sabine Maennel <sabine.maennel@gmail.com>
2023-02-23 08:23:07 +0100

    style: improve error message if px_id is not valid and provide a link to the FSO stattab webpage

--- R/fso_px_url.R
+++ R/fso_px_url.R
@@ -11,8 +11,8 @@ get_fso_px_download_url <- function(px_id) {
   download_url <- paste0(fso_px_url, px_id)
   is_px_cube_url <- check_px_cube_url(download_url)
   if (!is_px_cube_url) {
-    stop("This id does not belong to a px cube file, check STAT-TAB - interactive tables (FSO) for
-         valid px cube files.")
+    stop("This id does not belong to a px cube file, check https://www.bfs.admin.ch/bfs/en/home/services/recherche/stat-tab-online-data-search.html for
+         valid px cube ids.")
   }
   return(download_url)
 }
