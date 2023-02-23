d12e3cb11b7063b77e1cd6e9f9c81ffffda37db9
Sabine Maennel <sabine.maennel@gmail.com>
2023-02-23 07:37:10 +0100

    feature: check whether download url belongs to px cube
    The download url will be checked whether it belongs to a valid px cube file before it is returned.
    
    Co-authored-by: Laure Vancau <laurevanco@sfr.fr>

--- R/fso_px_url.R
+++ R/fso_px_url.R
@@ -9,5 +9,6 @@
 get_fso_px_download_url <- function(px_id) {
   fso_px_url <- 'https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file='
   download_url <- paste0(fso_px_url, px_id)
+  check_px_cube_url(download_url)
   return(download_url)
 }
